8e45f4801580f68bb8945d43f4b30a076115f616
EetuParvela <Eetu.Parvela@metropolia.fi>
2025-11-16 13:14:20 +0200

    Korjattu 2.2.py

--- Week2/2.2/2.2.py
+++ Week2/2.2/2.2.py
@@ -19,3 +19,41 @@ for _ in range(2500):
 
 
 def scale_values(data):
+    
+    min_val = min(data)
+    max_val = max(data)
+    new_val = 0
+    new_values = []
+    
+    for i in data:
+        new_val = round((i - min_val) * (100 / (max_val - min_val)))
+        new_values.append(new_val)
+        
+    return new_values
+
+
+oled.fill(0)
+oled.text("Task 2.2", 0, 0, 1)
+oled.text("press SW1", 0, 8, 1)
+oled.show()
+
+while True:
+    if not start_button.value():
+        oled.fill(0)
+        
+        values = scale_values(data_list)
+        text_pos_y = 64
+        text_pos_x = 0
+        
+        for i in values:
+            
+            text_pos_y -= 8
+            
+            oled.text(f"{i}", text_pos_x, text_pos_y, 1)
+            oled.hline(24, text_pos_y + 4, i, 1)
+            oled.show()
+            
+            if text_pos_y < oled_height - 8:
+                oled.scroll(0, -8)
+                oled.fill_rect(0, 56, 128, 8, 0)
+                text_pos_y = 56
